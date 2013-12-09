d69de5fadf80f05ae6fb8759b566a7c4c79a2eca
daemonicus666 <fredde.sephoria@gmail.com>
2013-12-09 13:46:16 +0100

    Ett enkelt script

---
+++ Ovning1/simpledialog.sh
@@ -0,0 +1,11 @@
+# Världens enklaste dialog
+# (c) Daemonicus666
+
+echo "Hello!\nType your name!"
+read year
+echo "Now, type your favourite color:"
+read color
+echo "What's your favourite food?"
+read food
+clear
+echo "Cool!\nYour name is: "$year"\nYour favourite color is: "$color"\nYour favourite food is: "$food
