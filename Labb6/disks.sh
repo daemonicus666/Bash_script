80a3610aa5746b7ebc7d66a186a39a1c71a724a0
daemonicus666 <fredde.sephoria@gmail.com>
2013-12-26 19:04:57 +0100

    Some disk usage

--- Labb6/disks.sh
+++ Labb6/disks.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-for dev in `df -H | awk '{ print $9 }' | egrep '[\/]'`;do
+
+warningRate=80
+
+# Get disks
+for dev in `df -H | awk '{ print $6 }' | egrep '[\/]'`;do
     Disk[$Nr]=$dev
-    echo $dev
+    ((Nr++))
 done
+
+# Get disk usage
+for percent in `df -H | awk ' { print $5 }' | egrep '[0-9]' | sed 's/̣\%//g'`;do
+    #Use[$UseNr]=$percent
+    #((UseNr++))
+    echo $percent
+done
+
+# Send some warnings etc.
+DiskDev=0
+
+for i in ${Use[@]};do
+    if [ $i -gt $warningRate ];then
+        echo "Disk ${Disk[$DiskDev]} is ${Use[$DiskDev]} full"
+        ((DiskDev++))
+    fi
+done
+
+exit 0
